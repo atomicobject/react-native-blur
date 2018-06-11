f99c8b7f2418c1c6cbf9f1072b17817383b201a7
Joe Chrysler <mail@joechrysler.com>
2018-06-11 09:12:02 -0400

    Disable the Android side of the project

--- android/src/main/java/com/cmcewen/blurview/BlurViewManager.java
+++ android/src/main/java/com/cmcewen/blurview/BlurViewManager.java
@@ -26,36 +26,23 @@ public class BlurViewManager extends SimpleViewManager<BlurringView> {
         context = ctx;
 
         BlurringView blurringView = new BlurringView(ctx);
-        blurringView.setBlurRadius(defaultRadius);
-        blurringView.setDownsampleFactor(defaultSampling);
         return blurringView;
     }
 
     @ReactProp(name = "blurRadius", defaultInt = defaultRadius)
     public void setRadius(BlurringView view, int radius) {
-        view.setBlurRadius(radius);
-        view.invalidate();
     }
 
     @ReactProp(name = "overlayColor", customType = "Color")
     public void setColor(BlurringView view, int color) {
-        view.setOverlayColor(color);
-        view.invalidate();
     }
 
     @ReactProp(name = "downsampleFactor", defaultInt = defaultSampling)
     public void setDownsampleFactor(BlurringView view, int factor) {
-        view.setDownsampleFactor(factor);
     }
 
     @ReactProp(name = "viewRef")
     public void setViewRef(BlurringView view, int viewRef) {
-        if (context != null && context.getCurrentActivity() != null) {
-          View viewToBlur = context.getCurrentActivity().findViewById(viewRef);
 
-          if (viewToBlur != null) {
-              view.setBlurredView(viewToBlur);
-          }
-        }
     }
 }
